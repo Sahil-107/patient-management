cd3a89e11ca76fcd148a28a17122b2dddc729304
Sahil-107 <messisahil7@gmail.com>
2026-05-22 21:13:11 +0530

    Everything is Working

---
+++ patient-service/src/main/java/com/pm/patient_service/dto/PatientRequestDTO.java
@@ -0,0 +1,30 @@
+package com.pm.patient_service.dto;
+
+import com.pm.patient_service.dto.validators.CreatePatientValidationGroup;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PatientRequestDTO {
+    @NotBlank
+    @Size(max=100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotNull(groups = CreatePatientValidationGroup.class, message = "Registered date is required")
+    private String registeredDate;
+}
---
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -0,0 +1,64 @@
+package com.pm.patient_service.service;
+
+import com.pm.patient_service.PatientServiceApplication;
+import com.pm.patient_service.dto.PatientRequestDTO;
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exceptions.EmailAlreadyExistsException;
+import com.pm.patient_service.exceptions.PatientNotFoundException;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
+import com.pm.patient_service.repository.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class PatientService {
+    private PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository){
+        this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+        log.info("patients {}", patients.getFirst().getName());
+        List<PatientResponseDTO> patientResponseDTOS = patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+        return patientResponseDTOS;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO){
+        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+            throw new EmailAlreadyExistsException("A Patient with this email already exists: "+ patientRequestDTO.getEmail());
+        }
+
+        Patient newPatient = patientRepository.save(PatientMapper.toModel(patientRequestDTO));
+        return PatientMapper.toDTO(newPatient);
+    }
+
+    public PatientResponseDTO updatePatient(UUID id, PatientRequestDTO patientRequestDTO){
+        Patient patient = patientRepository.findById(id).orElseThrow(
+                ()-> new PatientNotFoundException("Patient not found with ID: "+ id));
+
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)){
+            throw new EmailAlreadyExistsException("A Patient with this email already exists: "+ patientRequestDTO.getEmail());
+        }
+
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+
+        Patient updatePatient = patientRepository.save(patient);
+        return PatientMapper.toDTO(updatePatient);
+    }
+
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
+    }
+}
