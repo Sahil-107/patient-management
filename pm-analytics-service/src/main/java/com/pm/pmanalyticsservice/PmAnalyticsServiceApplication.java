2af98b32e1aec984de1d3248d08e069ad0d72b00
Sahil-107 <messisahil7@gmail.com>
2026-05-27 09:11:02 +0530

    added api gateway

---
+++ pm-analytics-service/src/main/java/com/pm/pmanalyticsservice/PmAnalyticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.pmanalyticsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmAnalyticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmAnalyticsServiceApplication.class, args);
+    }
+
+}
---
+++ pm-analytics-service/src/test/java/com/pm/pmanalyticsservice/PmAnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.pmanalyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmAnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
